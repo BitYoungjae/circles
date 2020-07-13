85fb132c9dc38e47563c1f3615123ddea9fc8817
consolekakao <console@kakao.com>
2020-07-13 16:47:14 +0900

    monday

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require("express"),
   bodyParser = require("body-parser");
 var app = express();
 var bodyparser = require("body-parser");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 var mysql = require("mysql");
 const cors = require("cors");
 var iconv = require("iconv-lite");
@@ -12,6 +14,7 @@ var cookieParser = require("cookie-parser");
 const FileStore = require("session-file-store")(session);
 const { response } = require("express");
 const { turquoise } = require("color-name");
+const { urlencoded } = require("body-parser");
 var connection = mysql.createConnection({
   host: "183.111.199.157",
   port: 3306,
@@ -58,7 +61,11 @@ app.post("/login", function (req, res) {
     }
   );
 });
-//a
+
+app.get("/reg", function (req, res) {
+  res.sendFile(__dirname + "/reg.html");
+});
+
 app.get("/sha", function (req, res) {
   res.sendFile(__dirname + "/sha256.html");
 });
@@ -66,4 +73,60 @@ app.post("/sha", function (req, res) {
   var str = req.body.str;
   console.log(str);
   var spdata = {};
+});
+
+app.post("/regcheck", function (req, res) {
+  //회원가입으로 pass 경우
+  var id = req.body.id; //넘어온 값들.
+  var pw = req.body.pw;
+  var nick = req.body.nick;
+  var spdata = {};
+  console.log(id);
+  res.send(id + "," + pw + "," + nick);
+  var query = connection.query(
+    'select * from nodeuser where id="' + id + '"', //해당 아이디가 존재하는지 조회
+    function (err, rows) {
+      if (err) throw err;
+      if (rows[0]) {
+        //값이 있으면 이미 있는 경우이므로 Fail.
+        spdata.result = "NO";
+        console.log("fail regist");
+        console.log("이미 등록된 아이디 입니다.");
+        console.log("----------------------");
+      } else {
+        //없는 경우 Seccess.
+        spdata.reslut = "ok";
+        console.log("seccess regist for " + spdata.reslut);
+        spdata.nick = encodeURI(rows[0]);
+        console.log("----------------------");
+        spdata.result = "OK";
+        var q = connection.query(
+          'insert into nodeuser values("' + //값 Insert.
+            id +
+            '","' +
+            pw +
+            '","' +
+            nick +
+            '") '
+        );
+
+        var query = connection.query(
+          'select * from nodeuser where id="' + id + '"', //해당 아이디가 존재하는지 조회
+          function (err, rows) {
+            if (err) throw err;
+            if (rows[0]) {
+              //값이 있으면 이미 있는 경우이므로 Fail.
+              spdata.result = "OK";
+              console.log("정상적으로 DB등록 완료.");
+              console.log("----------------------");
+            } else {
+              //없는 경우 Seccess.
+              spdata.reslut = "NO";
+              console.log("DB등록을 시도 했으나 문제가 발생했습니다.");
+            }
+          }
+        );
+      }
+    }
+  );
 });
